9f12f05f3723a6e66d994650ea8f8147e4967365
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2019-04-22 20:04:07 +0300

    most common words

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import nltk.corpus
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 
+import gensim
+print(dir(gensim))
+
 
 
 
@@ -24,7 +27,39 @@ if __name__ == '__main__':
 
 	print("Number of documents: " + str(len(raw_documents)))
 	
-	docs = [[w.lower() for w in word_tokenize(text)] 
-            for text in raw_documents]
+	gen_docs = [[w.lower() for w in word_tokenize(text)] 
+			for text in raw_documents]
 
-	print(docs)
+	print(gen_docs)
+
+
+	# dictionary = gensim.corpora.Dictionary(gen_docs)
+	# print(dictionary[5])
+	# print(dictionary.token2id['лет'])
+	# print("Number of words in dictionary:",len(dictionary))
+	# for i in range(len(dictionary)):
+	# 	print(i, dictionary[i])
+
+	#corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
+	#print(corpus)
+
+	#gen_docs = nltk.FreqDist(gen_docs)
+	words = nltk.tokenize.word_tokenize(raw_documents[0])
+	fdist = nltk.FreqDist(words)
+	print(fdist.most_common(4))
+
+
+	# query_doc_tf_idf = tf_idf[query_doc_bow]
+	# print(query_doc_tf_idf)
+
+
+	# tf_idf = gensim.models.TfidfModel(corpus)
+	# print(tf_idf)
+	# s = 0
+	# for i in corpus:
+	# 	s += len(i)
+	# print(s)
+
+	# sims = gensim.similarities.Similarity('~/upwork/similar/',tf_idf[corpus], num_features=len(dictionary))
+	# print(sims)
+	# print(type(sims))
