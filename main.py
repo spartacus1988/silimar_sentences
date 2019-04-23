276b06df12006205b004e9a7e30f2903ab869611
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2019-04-23 17:26:59 +0300

    autocommit

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-
-import nltk
-import nltk.text
-import nltk.corpus
-
-from nltk.tokenize import word_tokenize
-nltk.download('punkt')
-
-import gensim
-print(dir(gensim))
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
@@ -40,11 +29,13 @@ if __name__ == '__main__':
 	print("words", words)
 
 
-	n_clusters=3
+	n_clusters=5
 	number_of_seeds_to_try=10
 	max_iter = 300
 	number_of_process=2 # seads are distributed
 	model = KMeans(n_clusters=n_clusters, max_iter=max_iter, n_init=number_of_seeds_to_try, n_jobs=number_of_process).fit(X)
+	print(model)
+
 
 	labels = model.labels_
 	# indices of preferible words in each cluster
@@ -84,53 +75,3 @@ if __name__ == '__main__':
 
 
 
-	# print("Number of documents: " + str(len(raw_documents)))
-	
-	# gen_docs = [[w.lower() for w in word_tokenize(text)] 
-	# 		for text in raw_documents]
-
-	# print(gen_docs)
-
-
-
-	# dictionary = gensim.corpora.Dictionary(gen_docs)
-	# print(dictionary[5])
-	# print(dictionary.token2id['лет'])
-	# print("Number of words in dictionary:",len(dictionary))
-	# for i in range(len(dictionary)):
-	# 	print(i, dictionary[i])
-
-	#corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
-	#print(corpus)
-
-	#gen_docs = nltk.FreqDist(gen_docs)
-	#words = nltk.tokenize.word_tokenize(raw_documents[0])
-	#fdist = nltk.FreqDist(words)
-	#print(fdist.most_common(4))
-
-	# result_list = []
-
-	# for doc in gen_docs:
-	# 	fdist = nltk.FreqDist(doc)
-	# 	print(fdist.most_common(10))
-	# 	#result_list.append(fdist.most_common(10))
-	# 	result_list.append(fdist)
-
-	# print(result_list)
-
-
-
-	# query_doc_tf_idf = tf_idf[query_doc_bow]
-	# print(query_doc_tf_idf)
-
-
-	# tf_idf = gensim.models.TfidfModel(corpus)
-	# print(tf_idf)
-	# s = 0
-	# for i in corpus:
-	# 	s += len(i)
-	# print(s)
-
-	# sims = gensim.similarities.Similarity('~/upwork/similar/',tf_idf[corpus], num_features=len(dictionary))
-	# print(sims)
-	# print(type(sims))
